9098058bd1c84acfa26f0edb9fbc5bb770b361be
sanchitsaraswat <2023pietcssanchit145@poornima.org>
2026-04-17 00:01:56 +0530

    fix routing

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,9 +2,6 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-
-import path from "path";
-
 import { connectDB } from "./lib/db.js";
 
 import authRoutes from "./routes/auth.route.js";
@@ -13,33 +10,23 @@ import { app, server } from "./lib/socket.js";
 
 dotenv.config();
 
-const PORT = process.env.PORT;
-const __dirname = path.resolve();
+const PORT = process.env.PORT || 5000;
+
+// CORS FIX
+app.use(cors({
+  origin: "https://chat-app-ten-orcin-32.vercel.app",
+  credentials: true
+}));
 
 app.use(express.json());
-app.use(
-  cors({
-    origin: [
-      "http://localhost:5173",
-      "https://chat-app-ten-orcin-32.vercel.app"
-    ],
-    credentials: true,
-  })
-);
+app.use(cookieParser());
 
+// ROUTES
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-/* if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-  });
-}
-*/
-
+// START SERVER
 server.listen(PORT, () => {
-  console.log("server is running on PORT:" + PORT);
+  console.log("server running on PORT:", PORT);
   connectDB();
 });
