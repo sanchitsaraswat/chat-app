d1f769ac523830a0f9e24612e19d696866894ebf
sanchitsaraswat <2023pietcssanchit145@poornima.org>
2026-04-17 00:08:19 +0530

    fix routing

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,10 @@
 import axios from "axios";
 
+const BASE_URL =
+  import.meta.env.VITE_API_URL ||
+  "https://chat-app-backend-sigr.onrender.com";
+
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api",
+  baseURL: `${BASE_URL}/api`,
   withCredentials: true,
 });
